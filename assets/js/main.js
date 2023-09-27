784fe6c5f02dc5cb11707939f80ed3564ff6b8f3
HCruzWalook <hcruz@walook.com.mx>
2023-09-27 17:48:07 -0600

    base project

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    /*$(".nav-link").on("click", function(){
+        $(".nav-link.active").removeClass("active");
+        $(".nav-item.active").removeClass("active");
+        $(this).addClass("active");
+        $(this).parent().addClass("active");
+    });*/
+});
+/*document.addEventListener("DOMContentLoaded", function(){
+    var myOffcanvas = document.getElementById('offcanvasNavbar2');
+    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+    document.getElementById("OpenMenu").addEventListener('click',function (e){
+        e.preventDefault();
+        e.stopPropagation();
+        bsOffcanvas.toggle();
+    });
+});*/
